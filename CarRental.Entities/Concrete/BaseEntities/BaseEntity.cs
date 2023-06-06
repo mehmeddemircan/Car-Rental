e7de321b4f6654d7efff4e701fa7ed1a319b8f06
mehmet demircan <mehmeddemircan@gmail.com>
2023-06-06 12:28:38 +0300

    created BaseEntity

---
+++ CarRental.Entities/Concrete/BaseEntities/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿using CarRental.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental.Entities.Concrete.BaseEntities
+{
+    public class BaseEntity : IEntity
+    {
+        public int Id { get; set; }
+    }
+}

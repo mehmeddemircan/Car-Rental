bb72a1956c48f0e527395356b34ea1908299c9d5
mehmet demircan <mehmeddemircan@gmail.com>
2023-06-06 22:28:42 +0300

    created db context

---
+++ CarRental.DataAccess/Concrete/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental.DataAccess.Concrete.Contexts
+{
+    public class ApplicationDbContext : DbContext
+    {
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public ApplicationDbContext()
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\ProjectsV13;Database=CarRental;Trusted_Connection=true");
+        }
+
+
+
+    }
+}

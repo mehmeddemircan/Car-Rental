9b9bd0552ccacddc3820812d7fdbd3f109425128
mehmet demircan <mehmeddemircan@gmail.com>
2023-06-06 22:28:28 +0300

    added to core layer , base entity and dtos

---
+++ CarRental.Core/Entities/Abstract/IDisplayEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental.Entities.Abstract
+{
+    public interface IDisplayEntity
+    {
+        public int DisplayOrder { get; set; }
+
+        public bool IsDisplay { get; set; }
+
+    }
+}

2d2cc27d2a51dcfdd1e1bebd554dc3ff4a6a142c
mehmet demircan <mehmeddemircan@gmail.com>
2023-06-06 22:27:56 +0300

    create ıbase repository as interface

---
+++ CarRental.Core/DataAccess/IBaseRepository.cs
@@ -0,0 +1,25 @@
+﻿using CarRental.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental.DataAccess
+{
+    public interface IBaseRepository<TEntity> where TEntity : class , IEntity , new()
+    {
+
+        Task<IEnumerable<TEntity>> GetListAsync(Expression<Func<TEntity ,bool>> filter = null);
+
+        Task<TEntity> GetAsync(Expression<Func<TEntity, bool>> filter); 
+
+        Task<TEntity> AddAsync(TEntity entity);
+
+        Task<TEntity> UpdateAsync(TEntity entity);
+        
+        Task<bool> DeleteAsync(int id);
+
+    }
+}

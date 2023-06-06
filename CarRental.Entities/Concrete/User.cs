d7a474bc1f4638233ea62e229e120d593b6979fb
mehmet demircan <mehmeddemircan@gmail.com>
2023-06-06 12:28:48 +0300

    created User entity

---
+++ CarRental.Entities/Concrete/User.cs
@@ -0,0 +1,28 @@
+﻿using CarRental.Entities.Concrete.BaseEntities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental.Entities.Concrete
+{
+    public  class User : AuditableEntity
+    {
+
+        public string Username { get; set; }
+        public string Firstname { get; set; }
+
+        public string Lastname { get; set; }
+
+        public string Password { get; set; }
+
+        public bool Gender { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+
+        public string Email { get; set; }
+
+        public string Address { get; set; }
+    }
+}

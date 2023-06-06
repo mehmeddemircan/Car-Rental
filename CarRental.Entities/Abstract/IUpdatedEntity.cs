3c4493a4b62e8690d0b61bbc6a24baf3e57dbfc2
mehmet demircan <mehmeddemircan@gmail.com>
2023-06-06 12:28:21 +0300

    created IUpdatedEntity

---
+++ CarRental.Entities/Abstract/IUpdatedEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental.Entities.Abstract
+{
+    public interface IUpdatedEntity 
+    {
+
+        public int UpdatedBy { get; set; }
+
+        public DateTime UpdatedDate { get; set; }
+    }
+}

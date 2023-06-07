f349872d6ac338487f32c4794f9ed59c29285d9d
mehmet demircan <mehmeddemircan@gmail.com>
2023-06-07 12:30:00 +0300

    get list , get by ıd user functions created , manuelly mapper dto and user model

--- CarRental.Business/Concrete/UserManager.cs
+++ CarRental.Business/Concrete/UserManager.cs
@@ -2,6 +2,7 @@
 using CarRental.Business.Abstract;
 using CarRental.Core.Entities.Concrete.Auth;
 using CarRental.DataAccess.Abstract;
+using CarRental.Entities.DTOs.UserDtos;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -34,6 +35,48 @@ namespace  CarRental.Business.Concrete
         public User GetByMail(string email)
         {
             return _userRepository.Get(u => u.Email == email);
+        }
+
+        public async Task<IEnumerable<UserDetailDto>> GetListAsync()
+        {
+
+            List<UserDetailDto> userDetailDtos = new List<UserDetailDto>();
+
+              var response = await _userRepository.GetListAsync();
+            foreach (var item in response.ToList())
+            {
+
+                userDetailDtos.Add(new UserDetailDto()
+                {
+                    Id = item.Id,
+                    FirstName = item.FirstName,
+                    LastName = item.LastName,
+                    Email = item.Email,
+                    Status = item.Status
+                });
+
+            }
+            return userDetailDtos; 
+                
+        }
+
+        public async Task<UserDto> GetByIdAsync(int id)
+        {
+           var user = await _userRepository.GetAsync(x => x.Id == id);
+            UserDto userDto = new UserDto()
+            {
+                Id = user.Id,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Email = user.Email,
+                Status = user.Status
+            };
+            return userDto; 
+        }
+
+        public Task<bool> DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
         }
     }
 }

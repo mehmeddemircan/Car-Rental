24da96811c744f664399ebb149a0213b3d55e0e8
mehmet demircan <mehmeddemircan@gmail.com>
2023-06-07 22:05:20 +0300

    route attribute added to users controller

--- CarRental.WebAPI/Controllers/UsersController.cs
+++ CarRental.WebAPI/Controllers/UsersController.cs
@@ -10,15 +10,15 @@ namespace CarRental.WebAPI.Controllers
     public class UsersController : ControllerBase
     {
 
-        IUserService _userService; 
+        IUserService _userService;
         public UsersController(IUserService userService)
         {
             _userService = userService;
         }
 
-        [HttpGet("get-users")]
-
-        public  async Task<IActionResult> GetUsers()
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<IActionResult> GetUsers()
         {
             var result = await _userService.GetListAsync();
             if (result != null)
@@ -30,8 +30,8 @@ namespace CarRental.WebAPI.Controllers
 
         }
 
-        [HttpGet("users/{userId}")]
-
+        [HttpGet]
+        [Route("[action]/{userId:int}")]
         public async Task<IActionResult> GetUserById(int userId)
         {
             var result = await _userService.GetByIdAsync(userId);
@@ -48,16 +48,16 @@ namespace CarRental.WebAPI.Controllers
         public async Task<IActionResult> DeletePerson(int userId)
         {
             var result = await _userService.DeleteAsync(userId);
-            if (result)
+            if (result.Success)
             {
-                return Ok(result);
+                return Ok(result.Message);
             }
             return BadRequest();
         }
 
         [HttpPut("update-user")]
 
-        public async Task<IActionResult> UpdatePerson(UserUpdateDto userUpdateDto)
+        public async Task<IActionResult> UpdatePerson([FromBody] UserUpdateDto userUpdateDto)
         {
             var result = await _userService.UpdateAsync(userUpdateDto);
             if (result != null)

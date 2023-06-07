268ba8c73779df8175ff25fcbd854b9598c29ca7
mehmet demircan <mehmeddemircan@gmail.com>
2023-06-07 12:30:30 +0300

    userdtos designed according to user model

--- CarRental.Entities/DTOs/UserDtos/UserUpdateDto.cs
+++ CarRental.Entities/DTOs/UserDtos/UserUpdateDto.cs
@@ -12,19 +12,18 @@ namespace CarRental.Entities.DTOs.UserDtos
     {
 
         public int Id { get; set; }
-        public string Username { get; set; }
-        public string Firstname { get; set; }
+        public string FirstName { get; set; }
 
-        public string Lastname { get; set; }
+        public string LastName { get; set; }
 
         public string Password { get; set; }
-
-        public bool Gender { get; set; }
-
-        public DateTime DateOfBirth { get; set; }
-
         public string Email { get; set; }
 
-        public string Address { get; set; }
+        public bool Status { get; set; }
+
+        //public bool Gender { get; set; }
+
+        //public DateTime DateOfBirth { get; set; }
+        //public string Address { get; set; }
     }
 }

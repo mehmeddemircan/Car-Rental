e7942c13d54dfb549d4d6ceb67c6ba38ae34585d
mehmet demircan <mehmeddemircan@gmail.com>
2023-06-07 12:31:23 +0300

    get list users , get by ıd user controller  are added into users controller

---
+++ CarRental.WebAPI/Controllers/UsersController.cs
@@ -0,0 +1,44 @@
+﻿using CarRental.Business.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarRental.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+
+        IUserService _userService; 
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet("get-users")]
+
+        public  async Task<IActionResult> GetUsers()
+        {
+            var result = await _userService.GetListAsync();
+            if (result != null)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest();
+
+        }
+
+        [HttpGet("users/{userId}")]
+
+        public async Task<IActionResult> GetUserById(int userId)
+        {
+            var result = await _userService.GetByIdAsync(userId);
+            if (result != null)
+            {
+                return Ok(result);
+            }
+            return BadRequest();
+        }
+    }
+}

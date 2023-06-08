c1d49cdd1b089cbcca1f893afc09e129054dd392
mehmet demircan <mehmeddemircan@gmail.com>
2023-06-08 12:36:50 +0300

    created home page

---
+++ CarRental-Frontend/frontend/src/pages/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import MainLayout from '../layouts/MainLayout'
+import CallToActionCard from '../components/sections/CallToActionCard'
+
+const HomePage = () => {
+  return (
+    <MainLayout>
+        <CallToActionCard />
+  
+    </MainLayout>
+  )
+}
+
+export default HomePage

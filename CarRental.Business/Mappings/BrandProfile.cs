e0019247de879da2239d076b1666dd9e514834ba
mehmet demircan <mehmeddemircan@gmail.com>
2023-06-08 23:22:59 +0300

    created brand automapper profile

---
+++ CarRental.Business/Mappings/BrandProfile.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using CarRental.Entities.Concrete;
+using CarRental.Entities.DTOs.BrandDtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental.Business.Mappings
+{
+    public class BrandProfile : Profile
+    {
+
+        public BrandProfile()
+        {
+
+            CreateMap<BrandAddDto, Brand>();
+            CreateMap<Brand, BrandAddDto>();
+
+            CreateMap<BrandUpdateDto, Brand>();
+            CreateMap<Brand, BrandUpdateDto>();
+
+            CreateMap<BrandDetailDto, Brand>();
+            CreateMap<Brand,BrandDetailDto>();
+
+        }
+    }
+}

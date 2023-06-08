66588a0c23e0e07aed61c3bafc2b87b80a915777
mehmet demircan <mehmeddemircan@gmail.com>
2023-06-08 23:24:14 +0300

    created brand add dto

---
+++ CarRental.Entities/DTOs/BrandDtos/BrandAddDto.cs
@@ -0,0 +1,15 @@
+﻿using CarRental.Core.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental.Entities.DTOs.BrandDtos
+{
+    public class BrandAddDto : IDto
+    {
+
+        public string BrandName { get; set; }
+    }
+}

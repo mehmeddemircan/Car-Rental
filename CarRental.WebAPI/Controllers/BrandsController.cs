7d91122febf8eaf13d7957e85a4e41b78141c0aa
mehmet demircan <mehmeddemircan@gmail.com>
2023-06-08 23:26:00 +0300

    created brands controller for user

---
+++ CarRental.WebAPI/Controllers/BrandsController.cs
@@ -0,0 +1,47 @@
+﻿using CarRental.Business.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarRental.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BrandsController : ControllerBase
+    {
+
+        IBrandService _brandService; 
+        public BrandsController(IBrandService brandService)
+        {
+            _brandService = brandService;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+
+        public async Task<IActionResult> GetBrands()
+        {
+            var result = await _brandService.GetListAsync();
+            if (result != null)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest();
+
+        }
+
+
+        [HttpGet]
+        [Route("[action]/{brandId:int}")]
+        public async Task<IActionResult> GetBrandById(int brandId)
+        {
+            var result = await _brandService.GetByIdAsync(brandId);
+            if (result != null)
+            {
+                return Ok(result);
+            }
+            return BadRequest();
+        }
+
+    }
+}

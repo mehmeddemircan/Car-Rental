ef1080a30b21091a81ed57b40809d88820ce558a
mehmet demircan <mehmeddemircan@gmail.com>
2023-06-09 02:33:27 +0300

    created brand validator using fluend validation

---
+++ CarRental.Business/Validation/FluentValidation/BrandValidator.cs
@@ -0,0 +1,19 @@
+﻿using CarRental.Entities.DTOs.BrandDtos;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental.Business.Validation.FluentValidation
+{
+    public class BrandValidator : AbstractValidator<BrandAddDto>
+    {
+
+        public BrandValidator()
+        {
+            RuleFor(x => x.BrandName).MinimumLength(1).WithMessage("Geçersiz uzunlukta bir marka ismi girdiniz tekrar deneyiniz");
+        }
+    }
+}

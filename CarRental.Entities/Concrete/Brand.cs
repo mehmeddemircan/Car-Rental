a2dc3ff24b1e4a9ce7241af1e7027f519b763867
mehmet demircan <mehmeddemircan@gmail.com>
2023-06-08 23:24:06 +0300

    created brand model

---
+++ CarRental.Entities/Concrete/Brand.cs
@@ -0,0 +1,17 @@
+﻿using CarRental.Core.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental.Entities.Concrete
+{
+    public class Brand : AuditableEntity
+    {
+
+        public string BrandName { get; set; }
+
+        //public virtual ICollection<Model> CarModels { get; set; }
+    }
+}

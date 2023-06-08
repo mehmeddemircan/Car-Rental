65e027dd7c7a7b49aa134684ff801194c27d4796
mehmet demircan <mehmeddemircan@gmail.com>
2023-06-09 02:33:13 +0300

    created model profile

---
+++ CarRental.Business/Mappings/ModelProfile.cs
@@ -0,0 +1,32 @@
+﻿using AutoMapper;
+using CarRental.Entities.Concrete;
+using CarRental.Entities.DTOs.ModelDtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental.Business.Mappings
+{
+    public class ModelProfile : Profile
+    {
+
+        public ModelProfile()
+        {
+
+            CreateMap<ModelAddDto, Model>();
+            CreateMap<Model, ModelAddDto>();
+
+            CreateMap<ModelUpdateDto, Model>();
+            CreateMap<Model, ModelUpdateDto>();
+
+            CreateMap<ModelDetailDto, Model>();
+            CreateMap<Model, ModelDetailDto>();
+
+
+            CreateMap<ModelListDto, Model>();
+            CreateMap<Model, ModelListDto>();
+        }
+    }
+}

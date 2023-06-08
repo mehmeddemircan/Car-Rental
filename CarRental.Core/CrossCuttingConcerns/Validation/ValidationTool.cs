cec5826a17a4c101159218f40814ff26dbdc9db1
mehmet demircan <mehmeddemircan@gmail.com>
2023-06-09 02:34:09 +0300

    created validation tool

---
+++ CarRental.Core/CrossCuttingConcerns/Validation/ValidationTool.cs
@@ -0,0 +1,25 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental.Core.CrossCuttingConcerns.Validation
+{
+    public static class ValidationTool
+    {
+
+        public static void Validate(IValidator validator, object entity)
+        {
+            var context = new ValidationContext<object>(entity);
+            var result = validator.Validate(context);
+            if (!result.IsValid)
+            {
+                    var errorMessages = result.Errors.Select(error => error.ErrorMessage);
+                    var errorMessage = string.Join(", ", errorMessages);
+                    throw new ValidationException(errorMessage);
+            }
+        }
+    }
+}

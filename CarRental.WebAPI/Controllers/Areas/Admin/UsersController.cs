f36de79549dd0e3025e2ba7a709b639c39e45e4b
mehmet demircan <mehmeddemircan@gmail.com>
2023-06-08 23:25:51 +0300

    created users controller for admin added get all user controller

---
+++ CarRental.WebAPI/Controllers/Areas/Admin/UsersController.cs
@@ -0,0 +1,33 @@
+﻿using CarRental.Business.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarRental.WebAPI.Controllers.Areas.Admin
+{
+    [Area("Admin")]
+    [Route("api/[Area]/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        IUserService _userService; 
+        public UsersController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+
+        [HttpGet]
+        [Route("[action]")]
+        public async Task<IActionResult> GetUsers()
+        {
+            var result = await _userService.GetListAsync();
+            if (result != null)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest();
+
+        }
+    }
+}

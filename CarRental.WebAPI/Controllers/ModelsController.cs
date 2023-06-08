fc9523004ebe959a133fd64657686ad3f621aff3
mehmet demircan <mehmeddemircan@gmail.com>
2023-06-09 02:36:02 +0300

    created models controller for user

---
+++ CarRental.WebAPI/Controllers/ModelsController.cs
@@ -0,0 +1,46 @@
+﻿using CarRental.Business.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarRental.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ModelsController : ControllerBase
+    {
+
+        IModelService _modelService;
+        public ModelsController(IModelService modelService)
+        {
+            _modelService = modelService;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+
+        public async Task<IActionResult> GetModels()
+        {
+            var result = await _modelService.GetListAsync();
+            if (result != null)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest();
+
+        }
+
+
+        [HttpGet]
+        [Route("[action]/{modelId:int}")]
+        public async Task<IActionResult> GetModelById(int modelId)
+        {
+            var result = await _modelService.GetByIdAsync(modelId);
+            if (result != null)
+            {
+                return Ok(result);
+            }
+            return BadRequest();
+        }
+    }
+}

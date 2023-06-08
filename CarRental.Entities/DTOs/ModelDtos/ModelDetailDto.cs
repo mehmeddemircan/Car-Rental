56ea7aedc2849c664c03d5b750d5d0b7805eaa6c
mehmet demircan <mehmeddemircan@gmail.com>
2023-06-09 02:35:24 +0300

    created model detail dto

---
+++ CarRental.Entities/DTOs/ModelDtos/ModelDetailDto.cs
@@ -0,0 +1,20 @@
+﻿using CarRental.Core.Entities.Abstract;
+using CarRental.Entities.DTOs.BrandDtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental.Entities.DTOs.ModelDtos
+{
+    public class ModelDetailDto : IDto
+    {
+        public int Id { get; set; }
+        public string ModelName { get; set; }
+        
+        public BrandDetailDto Brand { get; set; }
+
+
+    }
+}

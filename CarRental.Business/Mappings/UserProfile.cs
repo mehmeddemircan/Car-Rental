657cdac23b23a54e3339c68765c86b24d3449762
mehmet demircan <mehmeddemircan@gmail.com>
2023-06-08 12:33:23 +0300

    created user profile for automapper

---
+++ CarRental.Business/Mappings/UserProfile.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using CarRental.Core.Entities.Concrete.Auth;
+using CarRental.Entities.DTOs.UserDtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental.Business.Mappings
+{
+    public class UserProfile : Profile
+    {
+
+        public UserProfile()
+        {
+            CreateMap<UserDetailDto, User>();
+            CreateMap<User, UserDetailDto>();
+
+            CreateMap<UserDto, User>();
+            CreateMap<User, UserDto>();
+
+            CreateMap<UserUpdateDto, User>();
+            CreateMap<User, UserUpdateDto>();
+
+        }
+        
+    }
+}

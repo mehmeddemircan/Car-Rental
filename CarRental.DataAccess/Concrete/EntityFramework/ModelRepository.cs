170673aefed4e07d06d1dd790803d039a2ecc58b
mehmet demircan <mehmeddemircan@gmail.com>
2023-06-09 02:34:36 +0300

    created model repository

---
+++ CarRental.DataAccess/Concrete/EntityFramework/ModelRepository.cs
@@ -0,0 +1,16 @@
+﻿using CarRental.Core.DataAccess.EntityFramework;
+using CarRental.DataAccess.Abstract;
+using CarRental.DataAccess.Concrete.Contexts;
+using CarRental.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental.DataAccess.Concrete.EntityFramework
+{
+    public class ModelRepository : EfBaseRepository<Model,ApplicationDbContext>, IModelRepository
+    {
+    }
+}

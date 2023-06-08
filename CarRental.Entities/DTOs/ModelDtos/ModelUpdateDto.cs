3978b6e955f12f73c9c629623ac1d9bd322ff0f0
mehmet demircan <mehmeddemircan@gmail.com>
2023-06-09 02:35:38 +0300

    created model update dto

---
+++ CarRental.Entities/DTOs/ModelDtos/ModelUpdateDto.cs
@@ -0,0 +1,16 @@
+﻿using CarRental.Core.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental.Entities.DTOs.ModelDtos
+{
+    public class ModelUpdateDto : IDto
+    {
+        public int Id { get; set; }
+        public string ModelName { get; set; }
+        public int BrandId { get; set; }
+    }
+}

b0b5d4bb71c1f031924d296e0e0abd06443c3575
mehmet demircan <mehmeddemircan@gmail.com>
2023-06-09 02:35:06 +0300

    created model class

---
+++ CarRental.Entities/Concrete/Model.cs
@@ -0,0 +1,17 @@
+﻿using CarRental.Core.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental.Entities.Concrete
+{
+    public class Model : AuditableEntity
+    {
+     
+        public string ModelName { get; set; }
+        public int BrandId { get; set; }
+        public Brand Brand { get; set; }
+    }
+}

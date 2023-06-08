7a05aad51ade4cf5a1298d7854292665ee3f35b6
mehmet demircan <mehmeddemircan@gmail.com>
2023-06-08 23:24:32 +0300

    created brand update dto

---
+++ CarRental.Entities/DTOs/BrandDtos/BrandUpdateDto.cs
@@ -0,0 +1,17 @@
+﻿using CarRental.Core.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental.Entities.DTOs.BrandDtos
+{
+    public class BrandUpdateDto : IDto
+    {
+
+        public int Id { get; set; }
+
+        public string BrandName { get; set; }
+    }
+}

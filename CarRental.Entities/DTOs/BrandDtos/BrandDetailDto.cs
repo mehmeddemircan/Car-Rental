caae363f555d85415ba4938c5aa4fca8731cf107
mehmet demircan <mehmeddemircan@gmail.com>
2023-06-08 23:24:23 +0300

    created brand detail dto

---
+++ CarRental.Entities/DTOs/BrandDtos/BrandDetailDto.cs
@@ -0,0 +1,18 @@
+﻿using CarRental.Core.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental.Entities.DTOs.BrandDtos
+{
+    public class BrandDetailDto  : IDto
+    {
+
+        public int Id { get; set; }
+
+        public string BrandName { get; set; }
+   
+    }
+}

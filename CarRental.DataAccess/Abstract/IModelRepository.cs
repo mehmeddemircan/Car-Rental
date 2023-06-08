e0e23f65e728f761240a85fccccf2c1dd9614f9d
mehmet demircan <mehmeddemircan@gmail.com>
2023-06-09 02:34:20 +0300

    created IModel repository

---
+++ CarRental.DataAccess/Abstract/IModelRepository.cs
@@ -0,0 +1,15 @@
+﻿using CarRental.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental.DataAccess.Abstract
+{
+    public interface IModelRepository : IBaseRepository<Model>
+    {
+
+
+    }
+}

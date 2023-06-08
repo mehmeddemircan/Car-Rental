cb7f09eefc0afbfcc606bbad2abbb8fa662f7aae
mehmet demircan <mehmeddemircan@gmail.com>
2023-06-09 02:33:40 +0300

    created model validator using fluent validation

---
+++ CarRental.Business/Validation/FluentValidation/ModelValidator.cs
@@ -0,0 +1,19 @@
+﻿using CarRental.Entities.DTOs.ModelDtos;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental.Business.Validation.FluentValidation
+{
+    public class ModelValidator : AbstractValidator<ModelAddDto>
+    {
+
+        public ModelValidator()
+        {
+            RuleFor(x => x.ModelName).MinimumLength(1).WithMessage("Geçersiz uzunlukta bir marka ismi girdiniz tekrar deneyiniz");
+        }
+    }
+}

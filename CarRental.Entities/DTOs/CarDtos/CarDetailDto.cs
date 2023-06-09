4df85ec76146c6d596607bf64e2183a5eeca5faf
mehmet demircan <mehmeddemircan@gmail.com>
2023-06-09 21:07:48 +0300

    created car detail dto

---
+++ CarRental.Entities/DTOs/CarDtos/CarDetailDto.cs
@@ -0,0 +1,26 @@
+﻿using CarRental.Core.Entities.Abstract;
+using CarRental.Entities.DTOs.ColorDtos;
+using CarRental.Entities.DTOs.ModelDtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental.Entities.DTOs.CarDtos
+{
+    public class CarDetailDto  : IDto
+    {
+
+        public int Id { get; set; }
+
+        public ModelDetailDto Model { get; set; }
+
+        public ColorDetailDto Color { get; set; }
+
+        public int Year { get; set; }
+
+        public decimal Price { get; set; }
+
+    }
+}

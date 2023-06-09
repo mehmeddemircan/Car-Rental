d64954453a9317622e3c1b6eed347a7e6af6c3c1
mehmet demircan <mehmeddemircan@gmail.com>
2023-06-09 11:50:01 +0300

    created cloudinary service

---
+++ CarRental.Business/Abstract/ICloudinaryService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental.Business.Abstract
+{
+    public interface ICloudinaryService
+    {
+        Task<string> UploadImageAsync(string imagePath);
+    }
+}

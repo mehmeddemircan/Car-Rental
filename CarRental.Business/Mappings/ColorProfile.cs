75728c2839ca997a98886f1bc82c3167751e4cc5
mehmet demircan <mehmeddemircan@gmail.com>
2023-06-09 19:17:41 +0300

    created color profile

---
+++ CarRental.Business/Mappings/ColorProfile.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using CarRental.Entities.Concrete;
+using CarRental.Entities.DTOs.ColorDtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental.Business.Mappings
+{
+    public class ColorProfile : Profile
+    {
+
+        public ColorProfile()
+        {
+
+            CreateMap<ColorAddDto, Color>();
+            CreateMap<Color, ColorAddDto>();
+
+            CreateMap<ColorUpdateDto, Color>();
+            CreateMap<Color, ColorUpdateDto>();
+
+            CreateMap<ColorDetailDto, Color>();
+            CreateMap<Color, ColorDetailDto>();
+
+        }
+    }
+}

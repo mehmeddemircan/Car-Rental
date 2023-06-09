74c460b69ec9fac459e84e484c64468832ebd215
mehmet demircan <mehmeddemircan@gmail.com>
2023-06-09 19:18:00 +0300

    created color validator using fluent validation

---
+++ CarRental.Business/Validation/FluentValidation/ColorValidator.cs
@@ -0,0 +1,20 @@
+﻿using CarRental.Entities.DTOs.ColorDtos;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental.Business.Validation.FluentValidation
+{
+    public class ColorValidator : AbstractValidator<ColorAddDto>
+    {
+
+        public ColorValidator()
+        {
+            RuleFor(x => x.ColorName).MinimumLength(1).WithMessage("Geçersiz uzunlukta bir marka ismi girdiniz tekrar deneyiniz");
+        }
+    }
+
+}

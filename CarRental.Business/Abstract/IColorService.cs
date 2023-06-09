045928c314131a7f2a6b38a8162029aa9debd382
mehmet demircan <mehmeddemircan@gmail.com>
2023-06-09 19:16:16 +0300

    created color service

---
+++ CarRental.Business/Abstract/IColorService.cs
@@ -0,0 +1,28 @@
+﻿using CarRental.Core.Utilities.Results;
+using CarRental.Entities.Concrete;
+using CarRental.Entities.DTOs.ColorDtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental.Business.Abstract
+{
+    public interface IColorService
+    {
+
+        Task<IDataResult<ColorDetailDto>> AddAsync(ColorAddDto entity);
+
+
+        Task<IDataResult<IEnumerable<ColorDetailDto>>> GetListAsync(Expression<Func<Color, bool>> filter = null);
+        Task<IDataResult<ColorDetailDto>> GetAsync(Expression<Func<Color, bool>> filter);
+
+        Task<IDataResult<ColorDetailDto>> GetByIdAsync(int id);
+
+        Task<IDataResult<ColorUpdateDto>> UpdateAsync(ColorUpdateDto modelUpdateDto);
+
+        Task<IDataResult<bool>> DeleteAsync(int id);
+    }
+}

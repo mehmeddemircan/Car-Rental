6dae2905752a9dba898b71f6a7da960506f792d0
mehmet demircan <mehmeddemircan@gmail.com>
2023-06-09 21:08:06 +0300

    created cars admin controller

---
+++ CarRental.WebAPI/Controllers/Areas/Admin/CarsController.cs
@@ -0,0 +1,63 @@
+﻿using CarRental.Business.Abstract;
+using CarRental.Entities.DTOs.CarDtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarRental.WebAPI.Controllers.Areas.Admin
+{
+    [Area("Admin")]
+    [Route("api/[Area]/[controller]")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+        ICarService _carService; 
+        public CarsController(ICarService carService)
+        {
+            _carService = carService;
+        }
+
+        [HttpPost]
+        [Route("[action]")]
+
+        public async Task<IActionResult> AddNewCar(CarAddDto colorAddDto)
+        {
+            var result = await _carService.AddAsync(colorAddDto);
+            if (result != null)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest();
+
+        }
+
+        [HttpDelete]
+        [Route("[action]/{carId:int}")]
+        public async Task<IActionResult> DeleteCar(int carId)
+        {
+            var result = await _carService.DeleteAsync(carId);
+
+            if (result.Success)
+            {
+                return Ok(result.Message);
+            }
+
+            return BadRequest();
+        }
+
+
+        [HttpPut]
+        [Route("[action]")]
+
+        public async Task<IActionResult> UpdateCar(CarUpdateDto carUpdateDto)
+        {
+            var result = await _carService.UpdateAsync(carUpdateDto);
+
+            if (result != null)
+            {
+                return Ok(result);
+            }
+            return BadRequest();
+        }
+    }
+}

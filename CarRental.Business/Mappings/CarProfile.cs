6fd2ffca50aafe47dc37607987820d1994a643f6
mehmet demircan <mehmeddemircan@gmail.com>
2023-06-09 21:06:55 +0300

    created car profile

---
+++ CarRental.Business/Mappings/CarProfile.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+using CarRental.Entities.Concrete;
+using CarRental.Entities.DTOs.CarDtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental.Business.Mappings
+{
+    public class CarProfile : Profile
+    {
+
+        public CarProfile()
+        {
+
+            CreateMap<CarAddDto, Car>();
+            CreateMap<Car, CarAddDto>();
+
+            CreateMap<CarUpdateDto, Car>();
+            CreateMap<Car, CarUpdateDto>();
+
+            CreateMap<CarDetailDto, Car>();
+            CreateMap<Car, CarDetailDto>();
+
+        }
+    }
+}

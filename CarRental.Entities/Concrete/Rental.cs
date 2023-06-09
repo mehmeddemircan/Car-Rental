45a709c329128bdf3d2a58367ddbac5230db08c2
mehmet demircan <mehmeddemircan@gmail.com>
2023-06-10 02:15:16 +0300

    created rental entity

---
+++ CarRental.Entities/Concrete/Rental.cs
@@ -0,0 +1,25 @@
+﻿using CarRental.Core.Entities.Concrete;
+using CarRental.Core.Entities.Concrete.Auth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental.Entities.Concrete
+{
+    public class Rental : AuditableEntity
+    {
+     
+         
+            public int CarId { get; set; }
+            public virtual Car Car { get; set; }
+            public int UserId { get; set; }
+
+            public virtual User User { get; set; }
+
+            public DateTime RentDate { get; set; }
+            public DateTime? ReturnDate { get; set; }
+        }
+    
+}

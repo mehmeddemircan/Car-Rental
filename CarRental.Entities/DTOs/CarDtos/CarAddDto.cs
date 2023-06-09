0ffe01c70a1807e0d663938e57774960b54913df
mehmet demircan <mehmeddemircan@gmail.com>
2023-06-09 21:07:39 +0300

    created car add dto

---
+++ CarRental.Entities/DTOs/CarDtos/CarAddDto.cs
@@ -0,0 +1,18 @@
+﻿using CarRental.Core.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental.Entities.DTOs.CarDtos
+{
+    public class CarAddDto : IDto
+    {
+        public int BrandId { get; set; }
+        public int ModelId { get; set; }
+        public int ColorId { get; set; }
+        public int Year { get; set; }
+        public decimal Price { get; set; }
+    }
+}

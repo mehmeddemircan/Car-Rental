6618cb7167693aed325260ff4e88798709282d76
mehmet demircan <mehmeddemircan@gmail.com>
2023-06-09 21:06:46 +0300

    created car validator to validate

---
+++ CarRental.Business/Validation/FluentValidation/CarValidator.cs
@@ -0,0 +1,19 @@
+﻿using CarRental.Entities.DTOs.CarDtos;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental.Business.Validation.FluentValidation
+{
+    public class CarValidator : AbstractValidator<CarAddDto>
+    {
+
+        public CarValidator()
+        {
+            RuleFor(x => x.Price).NotEmpty().WithMessage("Fiyat boş gecilemez !");
+        }
+    }
+}

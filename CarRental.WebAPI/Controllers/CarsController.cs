8bbdd2156a4495e0529610112bb8686ac5bcf98b
mehmet demircan <mehmeddemircan@gmail.com>
2023-06-09 21:08:16 +0300

    created cars controller for user

---
+++ CarRental.WebAPI/Controllers/CarsController.cs
@@ -0,0 +1,46 @@
+﻿using CarRental.Business.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarRental.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+
+        ICarService _carService;
+        public CarsController(ICarService carService)
+        {
+            _carService = carService;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+
+        public async Task<IActionResult> GetCars()
+        {
+            var result = await _carService.GetListAsync();
+            if (result != null)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest();
+
+        }
+
+
+        [HttpGet]
+        [Route("[action]/{carId:int}")]
+        public async Task<IActionResult> GetCarById(int carId)
+        {
+            var result = await _carService.GetByIdAsync(carId);
+            if (result != null)
+            {
+                return Ok(result);
+            }
+            return BadRequest();
+        }
+    }
+}

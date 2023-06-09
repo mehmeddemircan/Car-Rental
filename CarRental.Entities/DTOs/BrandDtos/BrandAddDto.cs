f9261df8d7e69c6a76a1d09fb97a073d5bfdbf77
mehmet demircan <mehmeddemircan@gmail.com>
2023-06-09 11:51:42 +0300

    added image to brand add dto

--- CarRental.Entities/DTOs/BrandDtos/BrandAddDto.cs
+++ CarRental.Entities/DTOs/BrandDtos/BrandAddDto.cs
@@ -1,4 +1,5 @@
 ﻿using CarRental.Core.Entities.Abstract;
+using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,5 +12,7 @@ namespace CarRental.Entities.DTOs.BrandDtos
     {
 
         public string BrandName { get; set; }
+
+        public IFormFile Image { get; set; }
     }
 }

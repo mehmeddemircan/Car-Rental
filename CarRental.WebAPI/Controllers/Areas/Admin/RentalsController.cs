ced1e66c5af6a7808ead8a4a40364ad532d14298
mehmet demircan <mehmeddemircan@gmail.com>
2023-06-10 02:16:19 +0300

    created rentals controller for admin

---
+++ CarRental.WebAPI/Controllers/Areas/Admin/RentalsController.cs
@@ -0,0 +1,66 @@
+﻿using CarRental.Business.Abstract;
+using CarRental.Entities.DTOs.RentalDtos;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarRental.WebAPI.Controllers.Areas.Admin
+{
+    [Area("Admin")]
+    [Route("api/[Area]/[controller]")]
+    [ApiController]
+    public class RentalsController : ControllerBase
+    {
+
+        IRentalService _rentalService;
+
+        public RentalsController(IRentalService rentalService)
+        {
+            _rentalService = rentalService;
+        }
+
+        [HttpPost]
+        [Route("[action]")]
+
+        public async Task<IActionResult> AddNewRental(RentalAddDto rentalAddDto)
+        {
+            var result = await _rentalService.AddAsync(rentalAddDto);
+            if (result != null)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest();
+
+        }
+
+        [HttpDelete]
+        [Route("[action]/{rentalId:int}")]
+        public async Task<IActionResult> DeleteRental(int rentalId)
+        {
+            var result = await _rentalService.DeleteAsync(rentalId);
+
+            if (result.Success)
+            {
+                return Ok(result.Message);
+            }
+
+            return BadRequest();
+        }
+
+
+        [HttpPut]
+        [Route("[action]")]
+
+        public async Task<IActionResult> UpdateRental(RentalUpdateDto rentalUpdateDto)
+        {
+            var result = await _rentalService.UpdateAsync(rentalUpdateDto);
+
+            if (result != null)
+            {
+                return Ok(result);
+            }
+            return BadRequest();
+        }
+
+    }
+}

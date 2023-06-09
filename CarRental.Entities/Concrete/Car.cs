b585bd19b42c35ed0ded9d1126d4c9234b49e924
mehmet demircan <mehmeddemircan@gmail.com>
2023-06-09 21:07:33 +0300

    created car entity

---
+++ CarRental.Entities/Concrete/Car.cs
@@ -0,0 +1,27 @@
+﻿using CarRental.Core.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental.Entities.Concrete
+{
+    public class Car : AuditableEntity
+    {
+
+        
+        public int ModelId { get; set; }
+
+        public Model Model { get; set; }
+
+        public int ColorId { get; set; }
+
+        public Color Color { get; set; }
+
+        public int Year { get; set; }
+        public decimal Price { get; set; }
+
+
+    }
+}

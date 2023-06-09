546227fae606cf2d03389d6d13e0bfab0b6641e9
mehmet demircan <mehmeddemircan@gmail.com>
2023-06-10 02:16:05 +0300

    created rental update dto

---
+++ CarRental.Entities/DTOs/RentalDtos/RentalUpdateDto.cs
@@ -0,0 +1,19 @@
+﻿using CarRental.Core.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental.Entities.DTOs.RentalDtos
+{
+    public class RentalUpdateDto : IDto
+    {
+
+        public int Id { get; set; }
+        public int CarId { get; set; }
+        public int UserId { get; set; }
+        public DateTime RentDate { get; set; }
+        public DateTime? ReturnDate { get; set; }
+    }
+}

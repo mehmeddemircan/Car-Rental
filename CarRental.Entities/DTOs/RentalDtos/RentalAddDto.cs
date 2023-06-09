fc026cee46923d9881870c403b50c6caeac0d91d
mehmet demircan <mehmeddemircan@gmail.com>
2023-06-10 02:15:48 +0300

    created rental add dto

---
+++ CarRental.Entities/DTOs/RentalDtos/RentalAddDto.cs
@@ -0,0 +1,17 @@
+﻿using CarRental.Core.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental.Entities.DTOs.RentalDtos
+{
+    public class RentalAddDto  : IDto
+    {
+
+        public int CarId { get; set; }
+        public int UserId { get; set; }
+        public DateTime RentDate { get; set; }
+    }
+}

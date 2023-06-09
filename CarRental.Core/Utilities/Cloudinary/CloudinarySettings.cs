5c52e523f22ecae98d0fc976be6245b440d16593
mehmet demircan <mehmeddemircan@gmail.com>
2023-06-09 11:51:15 +0300

    created cloudinary settings class in utilities folder

---
+++ CarRental.Core/Utilities/Cloudinary/CloudinarySettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental.Core.Utilities.Cloudinary
+{
+    public class CloudinarySettings
+    {
+        public string CloudName { get; set; }
+        public string ApiKey { get; set; }
+        public string ApiSecret { get; set; }
+    }
+}

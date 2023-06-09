c803370d3bae96fd3b03eaa44e67ac010c807308
mehmet demircan <mehmeddemircan@gmail.com>
2023-06-09 19:20:38 +0300

    added color table to database using migration code first approach

--- CarRental.DataAccess/Migrations/20230609161255_addColorTableToDatabase.Designer.cs
+++ CarRental.DataAccess/Migrations/20230609161255_addColorTableToDatabase.Designer.cs
@@ -4,6 +4,7 @@ using CarRental.DataAccess.Concrete.Contexts;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace CarRental.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230609161255_addColorTableToDatabase")]
+    partial class addColorTableToDatabase
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

60cbde2d655e93e8df3795d66160bb61188704e9
mehmet demircan <mehmeddemircan@gmail.com>
2023-06-09 19:19:12 +0300

    created color entity

---
+++ CarRental.Entities/Concrete/Color.cs
@@ -0,0 +1,15 @@
+﻿using CarRental.Core.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental.Entities.Concrete
+{
+    public class Color : AuditableEntity
+    {
+
+        public string ColorName { get; set; }
+    }
+}

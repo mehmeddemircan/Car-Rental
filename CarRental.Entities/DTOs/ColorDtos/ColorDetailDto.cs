1821530e05edee5e2290641d4a735b87b7809f07
mehmet demircan <mehmeddemircan@gmail.com>
2023-06-09 19:19:33 +0300

    created color detail dto

---
+++ CarRental.Entities/DTOs/ColorDtos/ColorDetailDto.cs
@@ -0,0 +1,17 @@
+﻿using CarRental.Core.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental.Entities.DTOs.ColorDtos
+{
+    public class ColorDetailDto : IDto
+    {
+
+        public int Id { get; set; }
+
+        public string ColorName { get; set; }
+    }
+}

1ff9b54faa77b70e0249da3756ede506f4134c7c
mehmet demircan <mehmeddemircan@gmail.com>
2023-06-09 19:20:21 +0300

    created colors controller to see all color and get by ıd

---
+++ CarRental.WebAPI/Controllers/ColorsController.cs
@@ -0,0 +1,49 @@
+﻿using CarRental.Business.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarRental.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ColorsController : ControllerBase
+    {
+
+        IColorService _colorService;
+
+        public ColorsController(IColorService colorService)
+        {
+            _colorService = colorService;
+        }
+
+
+        [HttpGet]
+        [Route("[action]")]
+
+        public async Task<IActionResult> GetColors()
+        {
+            var result = await _colorService.GetListAsync();
+            if (result != null)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest();
+
+        }
+
+
+        [HttpGet]
+        [Route("[action]/{colorId:int}")]
+        public async Task<IActionResult> GetColorById(int colorId)
+        {
+            var result = await _colorService.GetByIdAsync(colorId);
+            if (result != null)
+            {
+                return Ok(result);
+            }
+            return BadRequest();
+        }
+
+    }
+}

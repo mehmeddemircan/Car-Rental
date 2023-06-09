d41a78bb8d440fe2ff2e0086eefaacf7154abe5a
mehmet demircan <mehmeddemircan@gmail.com>
2023-06-09 19:19:21 +0300

    created color add dto

---
+++ CarRental.Entities/DTOs/ColorDtos/ColorAddDto.cs
@@ -0,0 +1,15 @@
+﻿using CarRental.Core.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental.Entities.DTOs.ColorDtos
+{
+    public class ColorAddDto : IDto
+    {
+
+        public string ColorName { get; set; }
+    }
+}

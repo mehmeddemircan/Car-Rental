153ca01ec1c36e2ae1c77c8656d3c56ac140832f
mehmet demircan <mehmeddemircan@gmail.com>
2023-06-10 17:27:46 +0300

    created package profile for automapper

---
+++ CarRental.Business/Mappings/PackageProfile.cs
@@ -0,0 +1,27 @@
+﻿using AutoMapper;
+using CarRental.Entities.Concrete;
+using CarRental.Entities.DTOs.PackageDtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental.Business.Mappings
+{
+    public class PackageProfile : Profile
+    {
+
+        public PackageProfile()
+        {
+            CreateMap<PackageAddDto, Package>();
+            CreateMap<Package, PackageAddDto>();
+
+            CreateMap<PackageUpdateDto, Package>();
+            CreateMap<Package, PackageUpdateDto>();
+
+            CreateMap<PackageDetailDto, Package>();
+            CreateMap<Package, PackageDetailDto>();
+        }
+    }
+}

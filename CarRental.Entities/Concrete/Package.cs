d65a00d245b23c9fe4a1339652fcfbf9c9375dbb
mehmet demircan <mehmeddemircan@gmail.com>
2023-06-10 17:28:37 +0300

    created package entity

---
+++ CarRental.Entities/Concrete/Package.cs
@@ -0,0 +1,17 @@
+﻿using CarRental.Core.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental.Entities.Concrete
+{
+    public class Package  : AuditableEntity
+    {
+
+        public string PackageName { get; set; }
+
+        public int ModelId { get; set; }
+    }
+}

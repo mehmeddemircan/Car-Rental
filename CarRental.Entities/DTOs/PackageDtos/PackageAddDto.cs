7fa70bf49272cc31e7e29c52c2b70433b7ed57e9
mehmet demircan <mehmeddemircan@gmail.com>
2023-06-10 17:28:45 +0300

    created package add dto

---
+++ CarRental.Entities/DTOs/PackageDtos/PackageAddDto.cs
@@ -0,0 +1,17 @@
+﻿using CarRental.Core.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental.Entities.DTOs.PackageDtos
+{
+    public class PackageAddDto : IDto
+    {
+
+        public string PackageName { get; set; }
+
+        public int ModelId { get; set; }
+    }
+}

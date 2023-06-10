ca4995db3b01c370f48b9896d262489bae077411
mehmet demircan <mehmeddemircan@gmail.com>
2023-06-10 17:29:29 +0300

    created packages controller for user

---
+++ CarRental.WebAPI/Controllers/PackagesController.cs
@@ -0,0 +1,33 @@
+﻿using CarRental.Business.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarRental.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PackagesController : ControllerBase
+    {
+        IPackageService _packageService; 
+
+        public PackagesController(IPackageService packageService)
+        {
+            _packageService = packageService;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+
+        public async Task<IActionResult> GetPackagesByModel(int modelId )
+        {
+            var result = await _packageService.GetListAsync(x => x.ModelId == modelId);
+            if (result != null)
+            {
+                return Ok(result);
+            }
+
+            return BadRequest();
+
+        }
+    }
+}

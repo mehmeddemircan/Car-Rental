ce39bdf2d1d94bcb48201a5eb813fe82712a0cd0
mehmet demircan <mehmeddemircan@gmail.com>
2023-06-10 17:28:00 +0300

    created package validator

---
+++ CarRental.Business/Validation/FluentValidation/PackageValidator.cs
@@ -0,0 +1,20 @@
+﻿using CarRental.Entities.DTOs.PackageDtos;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental.Business.Validation.FluentValidation
+{
+    public class PackageValidator : AbstractValidator<PackageAddDto>
+    {
+
+        public PackageValidator()
+        {
+            RuleFor(x => x.PackageName).NotEmpty().WithMessage("Paket ismi boş gecilemez ");
+        }
+    }
+
+}

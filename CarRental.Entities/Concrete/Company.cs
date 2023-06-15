eee2e3a57da91bc3684d6b12f434d69226269c64
mehmet demircan <mehmeddemircan@gmail.com>
2023-06-15 11:44:01 +0300

    created company entity

---
+++ CarRental.Entities/Concrete/Company.cs
@@ -0,0 +1,22 @@
+﻿using CarRental.Core.Entities.Concrete;
+using CarRental.Core.Entities.Concrete.Auth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarRental.Entities.Concrete
+{
+    public class Company : AuditableEntity
+    {
+
+        public string CompanyName { get; set; }
+
+        public int UserId { get; set; }
+
+        public virtual User User { get; set; }
+
+        public ICollection<Car> Cars { get; set; }
+    }
+}
